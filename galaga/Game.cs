9944a2a9e654aa616f5cfa41de2a93b68e78d373
Hanna Stokes <hannastokes@Hannas-MacBook-Pro.local>
2020-02-26 19:33:49 +0100

    ret

--- galaga/Game.cs
+++ galaga/Game.cs
@@ -2,16 +2,16 @@ using System;
 using DIKUArcade;
 using DIKUArcade.Timers;
 using DIKUArcade.EventBus;
+
 namespace galaga
 {
     public class Game : IGameEventProcessor<object> {
         private Window win;
-        int hey;
         private GameTimer gameTimer;
         public Game() {
         // TODO: Choose some reasonable values for the window and timer constructor. 
         // For the window, we recommend a 500x500 resolution (a 1:1 aspect ratio). win = new Window(... , ... , ...);
-        gameTimer = new  DIKUArcade.Timers.GameTimer(60, 60);
+        gameTimer = new  GameTimer(60, 60);
         win = new Window("Galaga" , 500 , 500);
         }
         public void GameLoop() {
