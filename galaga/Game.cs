82f42d82e05d1a5b97c3d5e4d8ca6e84df70068a
Daniel Stephensen! <danielstephensen@Daniels-MacBook-Pro.local>
2020-02-26 16:09:23 +0100

    projekt

--- galaga/Game.cs
+++ galaga/Game.cs
@@ -1,15 +1,18 @@
 using System;
 using DIKUArcade;
-
+using DIKUArcade.Timers;
+using DIKUArcade.EventBus;
 namespace galaga
 {
     public class Game : IGameEventProcessor<object> {
         private Window win;
-        private DIKUArcade.Timers.GameTimer gameTimer;
+        private int hey;
+        private GameTimer gameTimer;
         public Game() {
         // TODO: Choose some reasonable values for the window and timer constructor. 
         // For the window, we recommend a 500x500 resolution (a 1:1 aspect ratio). win = new Window(... , ... , ...);
-        gameTimer = new GameTimer(... , ...);
+        gameTimer = new  DIKUArcade.Timers.GameTimer(60, 60);
+        win = new Window("Galaga" , 500 , 500);
         }
         public void GameLoop() {
             while(win.IsRunning()) {
@@ -25,7 +28,8 @@ namespace galaga
                 }
  
                 if (gameTimer.ShouldReset()) {
-                    // 1 second has passed - display last captured ups and fps win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
+                    // 1 second has passed - display last captured ups and fps 
+                    win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
                     ", FPS: " + gameTimer.CapturedFrames; 
                 }
             }
