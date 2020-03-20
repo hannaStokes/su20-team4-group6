cfb5208a677e9dd855ac9f71db538e8ca066b54f
Daniel Stephensen! <danielstephensen@Daniels-MacBook-Pro.local>
2020-03-20 12:17:51 +0100

    h

---
+++ galaga/GalagaStates/MainMenu.cs
@@ -0,0 +1,21 @@
+using DIKUArcade.State;
+namespace galaga.GalagaStates {
+    public class MainMenu : IGameState {
+        private static MainMenu instance = null;
+        private Entity backGroundImage = new Entity (new DynamicShape(new Vec2F(0.0f, 0.0f), new Vec2F(0.0f, 0.0f)),new Image(Path.Combine("Assets", "Images", "TitleImage.png")));
+        private Text[] menuButtons = {new Text("New Game", new Vec2F(0.5f, 0.7f), new Vec2F(0.1f, 0.1f)), new Text("Quit", new Vec2F(0.5f, 0.5f), new Vec2F(0.1f, 0.1f))};
+        private int activeMenuButton;
+        private int maxMenuButtons;
+        public static MainMenu GetInstance() {
+            return MainMenu.instance ?? (MainMenu.instance = new MainMenu());
+        }
+        public void RenderState()
+        {
+            backGroundImage.RenderEntity();
+            foreach (Text menubottom in menuButtons)
+            {
+                menubottom.RenderText();
+            }
+        }
+    }
+}
--- galaga/GalagaStates/StateMachine.cs
+++ galaga/GalagaStates/StateMachine.cs
@@ -7,38 +7,31 @@ namespace galaga.GalagaStates {
         public StateMachine() {
             GalagaBus.GetBus().Subscribe(GameEventType.GameStateEvent, this);
             GalagaBus.GetBus().Subscribe(GameEventType.InputEvent, this);
-            ActiveState = MainMenu.GetInstance();
+            ActiveState = GameStateType.MainMenu.GetInstance();
         }
 
-        private void KeyPress(string key) {
-            switch(key) {
-                case "KEY_M":
-                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "MAIN_MENU", "", "");
-                    GalagaBus.GetBus().RegisterEvent(event);
-                    break;
-                case "KEY_R":
-                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "GAME_RUNNING", "", "");
-                    GalagaBus.GetBus().RegisterEvent(event);
-                    break;
-                case "KEY_P":
-                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "GAME_PAUSED", "", "");
-                    GalagaBus.GetBus().RegisterEvent(event);
-                    break;
-            }
+        void HandleKeyEvent(string keyValue, string keyAction) {
+            
         }
         private void SwitchState(GameStateType stateType) {
+            ActiveState = GameEventType.GameStateEvent;
             switch (stateType) {
                 case GameStateType.MainMenu :
-                    ActiveState = MainMenu.GetInstance();
-                    KeyPress("KEY_M")
+
+                    IGameState.HandleKeyEvent("KEY_M", "MAIN_MENU");
+                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "MAIN_MENU", "", "");
+                    GalagaBus.GetBus().RegisterEvent(event);
+                    TransformStringToState(event.Message);
                     break;
                 case GameStateType.GameRunning :
-                    ActiveState = MainMenu.GetInstance();
-                    KeyPress("KEY_R");
+                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "GAME_RUNNING", "", "");
+                    GalagaBus.GetBus().RegisterEvent(event);
+                    TransformStringToState(event.Message);
                     break;
                 case GameStateType.GamePaused :
-                    ActiveState = MainMenu.GetInstance();
-                    KeyPress("KEY_P");
+                    event = GameEventFactory<object>.CreateGameEventForAllProcessors(GameEventType.GameStateEvent, this, "GAME_PAUSED", "", "");
+                    GalagaBus.GetBus().RegisterEvent(event);
+                    TransformStringToState(event.Message);
                     break;
             }
         }
